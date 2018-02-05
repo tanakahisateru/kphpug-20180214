b874788b45f4c904560df704e6ef50446a3fa8ed
Hisateru Tanaka <tanakahisateru@gmail.com>
2018-02-05 22:43:50 +0900

    Create HTML view using Twig.

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class HelloWorldController extends Controller
 {
@@ -13,6 +12,13 @@ class HelloWorldController extends Controller
      */
     public function getIndex()
     {
-        return new Response('Welcome to your new controller!');
+        return $this->render('hello-world.html.twig', [
+            'title' => 'Hello World',
+            'message' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia deserunt mollit anim id est laborum.',
+        ]);
     }
 }
